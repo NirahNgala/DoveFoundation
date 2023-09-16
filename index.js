26659d922fb479ea76fee7a32522a513c8ea77cb
NirahNgala <115345194+NirahNgala@users.noreply.github.com>
2023-09-16 03:21:55 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+const parallaxElements = document.querySelector('.parallax');
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+
+  parallaxElements.forEach(element => {
+    const speed = element.getAttribute('data-speed');
+    const offset = -(scrollY * speed);
+    element.style.transform = 'translateY(${offset}px)';
+  });
+});
