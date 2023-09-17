ed3a52c8db31f17ca50f9ab7d616640b0adc53c3
NirahNgala <115345194+NirahNgala@users.noreply.github.com>
2023-09-17 13:49:52 +0300

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -24,3 +24,39 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  scrollbar: {
+    el: ".swiper-scrollbar", // Selector for the scrollbar element
+    hide: false, // Show the scrollbar
+    draggable: true, // Allow dragging the scrollbar
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
+
